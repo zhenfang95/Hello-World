e49f2732d8a675e90b5b21e127a438af290f54e6
fzf1995 <277717810@qq.com>
2020-04-30 17:21:33 +0800

    pytest

---
+++ mystudy/fzf_pytest/fixt01.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python 
+#-*- coding:utf-8 -*-
+# @time  :2020/4/30 15:31
+# @author:AlexFu
---
+++ mystudy/fzf_pytest/pytest分布式执行/web_conf_py/baidu/conftest.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python 
+#-*- coding:utf-8 -*-
+# @time  :2020/4/30 16:40
+# @author:AlexFu
