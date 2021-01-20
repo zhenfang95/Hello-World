df0bf129c28d2b62e6a48a99fb07ad8c15357509
username <email>
2021-01-20 20:18:00 +0800

    初学GUI

--- mystudy/GUI/Tkinter_py02.py
+++ mystudy/GUI/Tkinter_py02.py
@@ -15,11 +15,11 @@ class MY_GUI():
 
     #设置窗口
     def set_init_window(self):
-        self.init_window_name.title("文本处理工具_v1.2")           #窗口名
-        #self.init_window_name.geometry('320x160+10+10')                         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
+        self.init_window_name.title("tng加签工具_v1.0")           #窗口名
+        #self.init_window_name.geometry('320x160+10+10')         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
         self.init_window_name.geometry('1068x681+10+10')
-        #self.init_window_name["bg"] = "pink"                                    #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
-        #self.init_window_name.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
+        #self.init_window_name["bg"] = "pink"                    #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
+        #self.init_window_name.attributes("-alpha",0.9)          #虚化，值越小虚化程度越高
         #标签
         self.init_data_label = Label(self.init_window_name, text="待处理数据")
         self.init_data_label.grid(row=0, column=0)
@@ -39,10 +39,7 @@ class MY_GUI():
         self.log_data_Text = Text(self.init_window_name, width=66, height=9)  # 日志框
         self.log_data_Text.grid(row=13, column=12, columnspan=10)
         #按钮
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="ts加签名", bg="lightblue", width=10,command=self.str_trans_to_ts)  # 调用内部方法  加()为直接调用
-        self.str_trans_to_md5_button.grid(row=2, column=11)
-
-        self.str_trans_to_ts_button = Button(self.init_window_name, text="ts加签名_list", bg="lightblue", width=10,command=self.str_trans_ts_list)
+        self.str_trans_to_ts_button = Button(self.init_window_name, text="ts接口加签", bg="lightblue", width=10,command=self.str_trans_ts_list)
         self.str_trans_to_ts_button.grid(row=3, column=11)
 
         self.str_trans_copy_button = Button(self.init_window_name, text="复制结果", bg="lightblue", width=10,command=self.result_copy)
@@ -51,36 +48,26 @@ class MY_GUI():
     def result_copy(self):
         pyperclip.copy(self.result_data_Text.get(1.0,END))
 
-    #功能函数
-    def str_trans_to_ts(self):
-        rsa_private_key = self.mch_data_Text.get(1.0,END)
-        src = json.loads(self.init_data_Text.get(1.0,END).strip().replace("\n",""))
-        if src:
-            try:
-                singn = tng_signdata_rsa(private_key=rsa_private_key, sign_data=src)
-                #输出到界面
-                self.result_data_Text.delete(1.0,END)
-                self.result_data_Text.insert(1.0,singn)
-                self.write_log_to_Text("INFO:str_trans_to_ts success")
-            except:
-                self.result_data_Text.delete(1.0,END)
-                self.result_data_Text.insert(1.0,"ts加签失败")
-        else:
-            self.write_log_to_Text("ERROR:str_trans_to_ts failed")
-
     def str_trans_ts_list(self):
         rsa_private_key = self.mch_data_Text.get(1.0,END)
         src = json.loads(self.init_data_Text.get(1.0,END).strip().replace("\n",""))
         if src:
             try:
-                singn = tng_signdata_rsa_List(private_key=rsa_private_key, sign_data=src,redata_list='txnList')
-                #输出到界面
-                self.result_data_Text.delete(1.0,END)
-                self.result_data_Text.insert(1.0,singn)
-                self.write_log_to_Text("INFO:str_trans_to_ts success")
+                try:
+                    redata_list = src['txnList']
+                    singn = tng_signdata_rsa_List(private_key=rsa_private_key, sign_data=src,redata_list='txnList')
+                    #输出到界面
+                    self.result_data_Text.delete(1.0,END)
+                    self.result_data_Text.insert(1.0,singn)
+                    self.write_log_to_Text("INFO:str_trans_to_ts success")
+                except:
+                    singn = tng_signdata_rsa_List(private_key=rsa_private_key, sign_data=src)
+                    self.result_data_Text.delete(1.0, END)
+                    self.result_data_Text.insert(1.0, singn)
+                    self.write_log_to_Text("INFO:str_trans_to_ts success")
             except:
                 self.result_data_Text.delete(1.0,END)
-                self.result_data_Text.insert(1.0,"ts加签失败")
+                self.result_data_Text.insert(1.0,"加签名失败，请重试……")
         else:
             self.write_log_to_Text("ERROR:str_trans_to_ts failed")
 
@@ -103,39 +90,37 @@ class MY_GUI():
             self.log_data_Text.delete(1.0,2.0)
             self.log_data_Text.insert(END, logmsg_in)
 
-def tng_signdata_rsa(private_key,sign_data):
-    sign_data.pop('sign')  #删除字段sign
-    data_sort = dict(sorted(sign_data.items(),key=lambda x:x[0],reverse=False))  #对字典进行从小到大排序
-    #sign_data_url = urlencode(data_sort).encode('utf-8')
-    sign_data_1 = dictstr(data_sort).encode('utf-8')   #把待加密数据转成QueryString的格式
-    private_keyBytes = base64.b64decode(private_key)
-    priKey = RSA.importKey(private_keyBytes)
-    signer = PKCS1_v1_5.new(priKey)
-    hash_obj = SHA256.new()
-    hash_obj.update(sign_data_1)
-    #hash_obj.update(sign_data_url)
-    signature = base64.b64encode(signer.sign(hash_obj))
-    sign = signature.decode("utf-8")
-    sign_data['sign'] = sign
-    return json.dumps(sign_data,sort_keys=True, indent=2)   #格式化输出json数据
+def tng_signdata_rsa_List(private_key=None,sign_data=None,redata_list=None):
+    if redata_list == 'txnList':
+        listda = sign_data[redata_list]
+        sign_data_1 = dictstr_List(sign_data[redata_list][0])
+        sign_data[redata_list] = sign_data_1
+        sign_data.pop('sign')  #删除字段sign
+        data_sort = dict(sorted(sign_data.items(),key=lambda x:x[0],reverse=False))  #对字典进行从小到大排序
+        sign_data_2 = dictstr(data_sort).encode('utf-8')
+        private_keyBytes = base64.b64decode(private_key)
+        priKey = RSA.importKey(private_keyBytes)
+        signer = PKCS1_v1_5.new(priKey)
+        hash_obj = SHA256.new()
+        hash_obj.update(sign_data_2)
+        signature = base64.b64encode(signer.sign(hash_obj))
+        sign = signature.decode("utf-8")
+        sign_data['sign'] = sign
+        sign_data[redata_list] = listda
+    else:
+        sign_data.pop('sign')  # 删除字段sign
+        data_sort = dict(sorted(sign_data.items(), key=lambda x: x[0], reverse=False))  # 对字典进行从小到大排序
+        # sign_data_url = urlencode(data_sort).encode('utf-8')
+        sign_data_1 = dictstr(data_sort).encode('utf-8')  # 把待加密数据转成QueryString的格式
+        private_keyBytes = base64.b64decode(private_key)
+        priKey = RSA.importKey(private_keyBytes)
+        signer = PKCS1_v1_5.new(priKey)
+        hash_obj = SHA256.new()
+        hash_obj.update(sign_data_1)
+        signature = base64.b64encode(signer.sign(hash_obj))
+        sign = signature.decode("utf-8")
+        sign_data['sign'] = sign
 
-#带内嵌列表的参数加签
-def tng_signdata_rsa_List(private_key,sign_data,redata_list):
-    listda = sign_data[redata_list]
-    sign_data_1 = dictstr_List(sign_data[redata_list][0])
-    sign_data[redata_list] = sign_data_1
-    sign_data.pop('sign')  #删除字段sign
-    data_sort = dict(sorted(sign_data.items(),key=lambda x:x[0],reverse=False))  #对字典进行从小到大排序
-    sign_data_2 = dictstr(data_sort).encode('utf-8')
-    private_keyBytes = base64.b64decode(private_key)
-    priKey = RSA.importKey(private_keyBytes)
-    signer = PKCS1_v1_5.new(priKey)
-    hash_obj = SHA256.new()
-    hash_obj.update(sign_data_2)
-    signature = base64.b64encode(signer.sign(hash_obj))
-    sign = signature.decode("utf-8")
-    sign_data['sign'] = sign
-    sign_data[redata_list] = listda
     return json.dumps(sign_data,sort_keys=True, indent=2)
 
 def dictstr(dict1):
